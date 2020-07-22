3f2ff2192c6a9f93d25e4cb792d4d15fd296b6aa
sungsan <kunga077@gmail.com>
2020-07-22 22:21:02 +0900

    draft and authenticated

--- mysite/settings.py
+++ mysite/settings.py
@@ -121,3 +121,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', '.pythonanywhere.com']
+
+LOGIN_REDIRECT_URL = '/'
--- sungsani/views.py
+++ sungsani/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from .models import MyFriend, Post
 from django.utils import timezone
 from .forms import PostForm
+from django.contrib.auth.decorators import login_required
 
 def memo_index(request):
     return render(request, 'sungsani/memo_index.html', {})
@@ -22,19 +23,20 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'sungsani/post_detail.html', {'post': post})
 
+@login_required
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect(to='sungsani:post_detail', pk=post.pk)
     else:
         form = PostForm()
     return render(request, 'sungsani/post_new.html', {'form': form})
 
+@login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -42,10 +44,26 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.revised_date = timezone.now()
             post.save()
             return redirect('sungsani:post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
     return render(request, 'sungsani/post_new.html', {'form': form})
+
+@login_required
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'sungsani/post_draft_list.html', {'posts': posts})
+
+@login_required
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('sungsani:post_detail', pk=pk)
+
+@login_required
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('sungsani:post_list')
 # Create your views here.
